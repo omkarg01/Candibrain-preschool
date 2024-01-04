2d55ca2f51f90a4953f7a237e475c844f56dd24a
omkarg01 <omkargujja01@gmail.com>
2024-01-04 20:50:26 +0530

    done

--- footer.js
+++ footer.js
@@ -36,66 +36,66 @@ footerTemplate.innerHTML = `
   <script src="js/revolution-slider/js/rs6.min.js"></script>
   <script src="js/revolution-slider/extra-rev-slider1.js"></script>
 
-  <footer id="footer" class="footer divider layer-overlay overlay-dark-8"
-      data-tm-bg-img="https://via.placeholder.com/1920x834">
+  <footer id="footer" class="footer">
       <div class="footer-widget-area">
-        <div class="container pt-90 pb-40">
+        <div class="container pt-90 ">
           <div class="row">
             <div class="col-md-6 col-lg-6 col-xl-4">
-              <div class="widget tm-widget-contact-info contact-info-style1 contact-icon-theme-colored1">
-                <div class="thumb">
-                  <img alt="Logo" class="w-25" src="images/CandiBrain.png">
-                </div>
-                <div className="text-decoration font-size-15">
-                    <strong>
-                      Candibrain Preschool - an IITian's Initiative
-                    </strong>
-                  </div>
-                <div class="description">Vilankurchi, Saravanampatti, Coimbatore</div>
-                <ul class="mb-30">
-                  <li class="contact-phone">
-                    <div class="icon"><i class="flaticon-contact-042-phone-1"></i></div>
-                    <div class="text"><a href="tel:+91 8925353323">+91 8925353323</a></div>
-                  </li>
-                  <li class="contact-email">
-                    <div class="icon"><i class="flaticon-contact-043-email-1"></i></div>
-                    <div class="text"><a href="mailto:contact@candibrainpreschool.com">contact@candibrainpreschool.com</a></div>
-                  </li>
-                  <li class="contact-website">
-                    <div class="icon"><i class="flaticon-contact-035-website"></i></div>
-                    <div class="text"><a target="_blank" href="https://www.candibrainpreschool.com">www.candibrainpreschool.com</a></div>
-                  </li>
-                </ul>
+              <img class="mb-30" alt="Logo" width="100" src="images/CandiBrain.png">
+              <div className="text-decoration font-size-15">
+                <strong>
+                  Candibrain Preschool - an IITian's Initiative
+                </strong>
               </div>
             </div>
             <div class="col-md-6 col-lg-6 col-xl-3">
-              <div class="widget">
-                <h4 class="widget-title">Opening Hours</h4>
-                <div class="opening-hours border-dark">
+              <div class="widget ">
+                <h4 class="widget-title widget-title-line-bottom line-bottom-theme-colored1">Opening Hours</h4>
+                <div className="opening-hours border-dark col-12">
                   <ul>
-                    <li class="clearfix"> <span> Mon - Tues : </span>
-                      <div class="value"> 6.00 am - 10.00 pm </div>
+                    <li className="clearfix">
+                      <div className="value"><span> Mon - Fri : </span> 8.00 am - 8.00 pm </div>
                     </li>
-                    <li class="clearfix"> <span> Wednes - Thurs :</span>
-                      <div class="value"> 8.00 am - 6.00 pm </div>
-                    </li>
-                    <li class="clearfix"> <span> Fri :</span>
-                      <div class="value"> 3.00 pm - 8.00 pm </div>
-                    </li>
-                    <li class="clearfix"> <span> Sun : </span>
-                      <div class="value"> Closed </div>
+                    <li className="clearfix">
+                      <div className="value"><span> Sat - Sun : </span> Closed </div>
                     </li>
                   </ul>
                 </div>
               </div>
             </div>
+            <div class="col-md-6 col-lg-6 col-xl-1"></div>
+            <div class="col-md-6 col-lg-6 col-xl-4 p-0">
+              <div class="widget p-40" data-tm-bg-color="#192637">
+                <h4 class="widget-title">Contact</h4>
+                <ul class="tm-widget">
+                  <li><span class="fa fa-phone-square text-theme-colored1 mr-10"></span> <span> +91 8925353323</span>
+                  </li>
+                  <li><span class="fa fa-envelope text-theme-colored1 mr-10"></span> <span>
+                      contact@candibrainpreschool.com</span>
+                  </li>
+                  <li><span class="fa fa-map-marker text-theme-colored1 mr-10"></span> <span> Vilankurchi,
+                      Saravanampatti, Coimbatore</span></li>
+                </ul>
+              </div>
+            </div>
+          </div>
         </div>
-        <div class="footer-bottom" data-tm-bg-color="#2A2A2A">
+        <div class="footer-bottom" data-tm-bg-color="black">
           <div class="container">
             <div class="row pt-20 pb-20">
               <div class="col-sm-6">
-                <div class="footer-paragraph">
+                <div class="footer-paragraph text-center text-xl-left text-lg-start text-md-left mb-sm-30">
                   © 2023 Candibrain. All Rights Reserved.
+                </div>
+              </div>
+              <div class="col-sm-6">
+                <div class="footer-paragraph text-center text-xl-end text-lg-end text-md-end">
+                  <ul class="styled-icons icon-md icon-dark icon-hover-theme-colored1 icon-circled clearfix">
+                    <li><a class="social-link" href="#"><i class="fab fa-facebook"></i></a></li>
+                    <li><a class="social-link" href="#"><i class="fab fa-twitter"></i></a></li>
+                    <li><a class="social-link" href="#"><i class="fab fa-youtube"></i></a></li>
+                    <li><a class="social-link" href="#"><i class="fab fa-instagram"></i></a></li>
+                  </ul>
                 </div>
               </div>
             </div>
